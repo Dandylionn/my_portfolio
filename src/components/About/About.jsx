be587f263ab10d9ef5852526f2a9161eabfa474c
Kwek Zheng Hui <zheng.hui94@gmail.com>
2024-02-29 14:58:29 +0800

    added Experience component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
 
 export const About = () => {
   return (
-    <section>
-      <h2>About</h2>
-      <div>
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/foxy.png")}
           alt="Me sitting with a laptop"
+          className={styles.aboutImage}
         />
-        <ul>
-          <li>
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-            <div>
-              <h3>Frontend Developer</h3>
+            <div className={styles.aboutItemText}>
+              <h3>Software Engineer</h3>
               <p>
-                I'm a frontend developer with experience in building and
-                optimized sites
+                I'm a Software Engineer with experience in testing and
+                maintenance of a system module
               </p>
             </div>
           </li>
-          <li>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
-            <div>
-              <h3>Backend Developer</h3>
-              <p>I have experience in developing backend system and APIs</p>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor2 Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>System Integrator</h3>
+              <p>
+                I have experience in integrating multiple products into a system
+                for clients
+              </p>
             </div>
           </li>
-          <li>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-            <div>
-              <h3>UI Designer</h3>
-              <p>I have designed multiple landing pages</p>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor3 Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>I have developed backend system and APIs in a project</p>
             </div>
           </li>
         </ul>
